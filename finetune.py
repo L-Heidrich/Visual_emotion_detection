d52057881f19695bd12ddf65b7c00ba2f01db9f8
theirAspiraions <>
2023-05-21 22:39:36 +0200

    "Cleaning up code, finished finetune script"

--- finetune.py
+++ finetune.py
@@ -3,9 +3,25 @@ from torch.utils.data import DataLoader
 import torch.nn as nn
 import torchvision
 import torch.optim as optim
-import torch.nn.functional as F
 from data import get_data
-from torchvision.models import resnet50, ResNet50_Weights
+from torchvision.models import ResNet50_Weights
+from main import train
 
 model = torchvision.models.resnet50(weights=ResNet50_Weights)
 model.fc = nn.Linear(in_features=2048, out_features=8, bias=True)
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+train_ds, test_ds = get_data()
+
+train_loader = DataLoader(train_ds, batch_size=32)
+test_loader = DataLoader(train_ds, batch_size=32)
+
+model = model.to(device)
+epochs = 30
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(),
+                      lr=0.03)
+
+accs, losses = train(model, epochs, train_loader, test_loader, criterion, optimizer, device)
+torch.save(model.state_dict(), "./models/emotion_detection_model_resnet.pt")
--- main.py
+++ main.py
@@ -9,11 +9,7 @@ import torch.optim as optim
 from data import get_data
 import matplotlib.pyplot as plt
 from model_class import Model
-
-train_ds, test_ds = get_data()
-
-train_loader = DataLoader(train_ds, batch_size=32)
-test_loader = DataLoader(train_ds, batch_size=32)
+from datetime import datetime
 
 
 def calculate_accuracy(dataloader, model):
@@ -40,42 +36,56 @@ def calculate_accuracy(dataloader, model):
         acc = 100 * correct_images // total_images
         return acc
 
-model = Model().to("cuda")
-epochs = 30
 
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(model.parameters(),
-                      lr=0.03)
+def train(model, epochs, train_loader, test_loader, criterion, optimizer, device):
+    print(f"Started training at {datetime.now()}")
+    losses = []
+    accs = []
 
-losses = []
-accs = []
+    for epoch in range(epochs):
+        running_loss = 0.0
+        model = model.train()
+        for batch in train_loader:
+            optimizer.zero_grad()
 
-for epoch in range(epochs):
-    running_loss = 0.0
-    model = model.train()
-    for batch in train_loader:
-        optimizer.zero_grad()
+            images = batch["image"].to(device)
+            targets = batch["label"].to(device)
 
-        images = batch["image"].to("cuda")
-        targets = batch["label"].to("cuda")
+            out = model(images)
 
-        out = model(images)
+            loss = criterion(out, targets)
+            loss.backward()
+            running_loss += loss.item()
+            optimizer.step()
 
-        loss = criterion(out, targets)
-        loss.backward()
-        running_loss += loss.item()
-        optimizer.step()
+        acc = calculate_accuracy(test_loader, model)
+        loss = running_loss / len(train_loader)
+        losses.append(loss)
+        accs.append(acc)
+        if epoch % 10 == 0:
+            print(f"Epoch [{epoch}]: loss: {loss}, train acc {acc}%, Time: {datetime.now()}")
 
-    acc = calculate_accuracy(test_loader, model)
-    loss = running_loss / len(train_loader)
-    losses.append(loss)
-    accs.append(acc)
-    if epoch % 10 == 0:
-        print(f"Epoch [{epoch}]: loss: {loss}, train acc {acc} %")
+    return accs, losses
 
-torch.save(model.state_dict(), "./models/emotion_detection_model.pt")
 
-plt.plot(accs, label="Accuracy")
-plt.plot(losses, label="Loss")
-plt.legend()
-plt.show()
+if __name__ == "__main__":
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    train_ds, test_ds = get_data()
+
+    train_loader = DataLoader(train_ds, batch_size=32)
+    test_loader = DataLoader(train_ds, batch_size=32)
+
+    model = Model().to(device)
+    epochs = 30
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(),
+                          lr=0.03)
+
+    accs, losses = train(model, epochs, train_loader, test_loader, criterion, optimizer, device)
+    torch.save(model.state_dict(), "./models/emotion_detection_model.pt")
+
+    plt.plot(accs, label="Accuracy")
+    plt.plot(losses, label="Loss")
+    plt.legend()
+    plt.show()
