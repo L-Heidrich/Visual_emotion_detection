3c80b437d583f39db40ace683b89f75200dbd92e
Lenny <48755165+L-Heidrich@users.noreply.github.com>
2023-05-29 21:46:03 +0200

    "introduced validation set and model training over multiple hyper parameters"

--- data.py
+++ data.py
@@ -2,42 +2,43 @@ from datasets import load_dataset, Dataset
 from torchvision import transforms
 
 
-def get_data():
+def get_data(train_percentile, val_percentile, test_percentile):
     dataset = load_dataset("FastJobs/Visual_Emotional_Analysis", split="train")
 
     dataset = dataset.shuffle(seed=432)
-    train_ds = dataset[:640]
-    test_ds = dataset[640:]
 
-    label_count = [0, 0, 0, 0, 0, 0, 0, 0]
+    if (train_percentile + val_percentile + test_percentile) != 1:
+        print("Split distributions must sum up to one")
+    else:
+        train_idx = int(train_percentile * len(dataset))
+        val_idx = int(val_percentile * len(dataset))
+        test_idx = int(test_percentile * len(dataset))
 
-    for label in train_ds["label"]:
-        label_count[label] += 1
+        train_ds = dataset[:train_idx]
+        test_ds = dataset[train_idx:train_idx + test_idx]
+        val_ds = dataset[train_idx + test_idx:]
 
-    print(label_count)
-    my_transforms = transforms.Compose(
-        [
-            transforms.RandomHorizontalFlip(),
-            transforms.ToTensor()]
-    )
+        my_transforms = transforms.Compose(
+            [
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor()]
+        )
 
-    my_test_transforms = transforms.Compose(
-        [
-            transforms.ToTensor()]
-    )
+        my_test_transforms = transforms.Compose(
+            [
+                transforms.ToTensor()]
+        )
 
-    def apply_transforms(images):
-        images["image"] = [my_transforms(image) for image in images["image"]]
-        return images
+        def apply_transforms(images):
+            images["image"] = [my_transforms(image) for image in images["image"]]
+            return images
 
-    def apply_test_transforms(images):
-        images["image"] = [my_test_transforms(image) for image in images["image"]]
-        return images
+        def apply_test_transforms(images):
+            images["image"] = [my_test_transforms(image) for image in images["image"]]
+            return images
 
-    train_ds = Dataset.from_dict(train_ds).with_format("torch").with_transform(apply_transforms)
-    test_ds = Dataset.from_dict(test_ds).with_format("torch").with_transform(apply_test_transforms)
+        train_ds = Dataset.from_dict(train_ds).with_format("torch").with_transform(apply_transforms)
+        test_ds = Dataset.from_dict(test_ds).with_format("torch").with_transform(apply_test_transforms)
+        val_ds = Dataset.from_dict(val_ds).with_format("torch").with_transform(apply_test_transforms)
 
-    return train_ds, test_ds
-
-
-get_data()
+        return train_ds, val_ds, test_ds
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import itertools
+
 import torch
 from torch.utils.data import DataLoader
 import torch.nn as nn
@@ -8,7 +10,7 @@ from model_class import Model
 from datetime import datetime
 
 
-def calculate_accuracy(dataloader, model):
+def calculate_accuracy(dataloader, model, device):
     """
     :param dataloader: dataloader you want to measure the accuracy on
     :param model: Your model
@@ -20,8 +22,8 @@ def calculate_accuracy(dataloader, model):
     total_images = 0
     with torch.no_grad():
         for batch in dataloader:
-            images = batch["image"].to("cuda")
-            targets = batch["label"].to("cuda")
+            images = batch["image"].to(device)
+            targets = batch["label"].to(device)
             outputs = model(images)
             _, predicted = torch.max(outputs.data, 1)
             # print(targets, outputs)
@@ -33,55 +35,91 @@ def calculate_accuracy(dataloader, model):
         return acc
 
 
-def train(model, epochs, train_loader, test_loader, criterion, optimizer, device):
+def train(models_list, epochs_list, train_loader, val_loader, test_loader, criterion, learning_rates, device):
+    """
+    :param model: array of pytorch model classes
+    :param epochs: array of epochs numbers
+    :param train_loader: dataloader for training data
+    :param test_loader: dataloader for test data
+    :param val_loader: dataloader for validation data
+    :param criterion: loss function
+    :param learning_rates: learning rates to be used
+    :param device: where to train the model on. cuda or cpu
+    :return:
+    """
+
     print(f"Started training at {datetime.now()}")
-    losses = []
-    accs = []
 
-    for epoch in range(epochs):
-        running_loss = 0.0
-        model = model.train()
-        for batch in train_loader:
-            optimizer.zero_grad()
+    results = []
+    hyper_param_combinations = itertools.product(models_list, epochs_list, learning_rates)
+    for setup in hyper_param_combinations:
 
-            images = batch["image"].to(device)
-            targets = batch["label"].to(device)
+        model, epochs, lr = setup
 
-            out = model(images)
+        model_starting_checkpoint = model.state_dict()
+        model.load_state_dict(model_starting_checkpoint)
 
-            loss = criterion(out, targets)
-            loss.backward()
-            running_loss += loss.item()
-            optimizer.step()
+        print(f"Training with setup: [epochs]: {epochs}, [lr]: {lr}, [device]: {device}")
+        optimizer = optim.SGD(model.parameters(),
+                              lr=lr)
+        losses = []
+        accs = []
+        acc = 0
+        loss = 0
 
-        acc = calculate_accuracy(test_loader, model)
-        loss = running_loss / len(train_loader)
-        losses.append(loss)
-        accs.append(acc)
-        if epoch % 10 == 0:
+        for epoch in range(epochs):
+            running_loss = 0.0
+            model = model.train()
+
+            for batch in train_loader:
+                optimizer.zero_grad()
+
+                images = batch["image"].to(device)
+                targets = batch["label"].to(device)
+
+                out = model(images)
+
+                loss = criterion(out, targets)
+                loss.backward()
+                running_loss += loss.item()
+                optimizer.step()
+
+            acc = calculate_accuracy(test_loader, model, device)
+            loss = running_loss / len(train_loader)
+            losses.append(loss)
+            accs.append(acc)
             print(f"Epoch [{epoch}]: loss: {loss}, train acc {acc}%, Time: {datetime.now()}")
 
-    return accs, losses
+        results.append({
+            "accuracy": acc,
+            "epochs": epochs,
+            "lr": lr,
+        })
+
+        torch.save(model.state_dict(), f"./models/emotion_detection_model_epochs_{epochs}_accuracy_{acc}_lr_{lr}.pt")
+    return results
 
 
 if __name__ == "__main__":
     device = "cuda" if torch.cuda.is_available() else "cpu"
-    train_ds, test_ds = get_data()
+    train_ds, val_ds, test_ds = get_data(0.7, 0.15, 0.15)
 
     train_loader = DataLoader(train_ds, batch_size=32)
     test_loader = DataLoader(train_ds, batch_size=32)
+    val_loader = DataLoader(val_ds, batch_size=32)
 
     model = Model().to(device)
-    epochs = 30
+    epochs = [10, 20]
+    learning_rates = [0.03, 0.003, 0.3]
 
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(model.parameters(),
-                          lr=0.03)
+    results = train([model],
+                    epochs,
+                    learning_rates=learning_rates,
+                    train_loader=train_loader,
+                    test_loader=test_loader,
+                    val_loader=val_loader,
+                    criterion=criterion,
+                    device=device)
 
-    accs, losses = train(model, epochs, train_loader, test_loader, criterion, optimizer, device)
-    torch.save(model.state_dict(), "./models/emotion_detection_model.pt")
-
-    plt.plot(accs, label="Accuracy")
-    plt.plot(losses, label="Loss")
-    plt.legend()
-    plt.show()
+    print(results)
